ad3477bcb83c53317645b54c51565df2ae8219ac
Mateo Ruiz Diaz <MateoRD65@gmail.com>
2025-05-09 22:20:31 -0300

    Correccion de redundancias y limpieza del codigo. Adicion de nuevos comentarios haciendolo mas descriptivo y aportando algo de calma a la vista con la separaciones de codigo.

--- main.js
+++ main.js
@@ -14,30 +14,33 @@ let decisionDelUsuario;
 let valorARS;
 let precioUSD = 1300;
 let valorUSD;
-let arrayUSD = [{
-    tipo: "divisa",
-    entrada : valorARS,
-    salida : valorUSD,
-    fecha: new Date().toLocaleString()
-}];
-
-decisionDelUsuario = parseInt(decisionDelUsuario); //Convertir a número entero
+let arrayUSD = [];
 
 //Funciones:
 function convertCurrency(valorARS){ //Convertir de ARS a USD
     if(valorARS > 0 ){ 
         return valorARS / precioUSD; 
     }
-    valorUSD = convertCurrency(valorARS);
+    return 0;
 }
 
 //Bucle:
 do{ //Ejecuta el codigo al menos una vez.
-    decisionDelUsuario = parseInt(prompt('1) Divisa (ARS -> USD) 2) Salir.'));
-    if(decisionDelUsuario == 1){ //Si la decision del usuario es 1, Ingresa a la calculadora.
+
+    decisionDelUsuario = parseInt(prompt('1) Divisa.  (ARS => USD) \n2) Salir.'));
+
+    if(decisionDelUsuario == 1){ 
+        //Si la decision del usuario es 1, Ingresa a la calculadora.
+
+        //Prompt para ingresar el monto en ARS a convertir a USD.
         valorARS = parseFloat(prompt("Ingrese el monto en ARS que desea convertir a USD."));
-        if(valorARS > 0 ){ // Validacion para que el precio nunca sea negativo
+
+        // Validacion para que el precio nunca sea negativo
+        if(valorARS > 0 ){ 
             alert("El monto en USD es: " + convertCurrency(valorARS).toFixed(2) + " USD.");
+
+            // Guardar la operación en el array de objetos
+            // Se guarda el tipo de operación, el monto de entrada (ARS), el monto de salida (USD) y la fecha.
             arrayUSD.push({
                 tipo: "divisa",
                 entrada: valorARS,
@@ -47,11 +50,8 @@ do{ //Ejecuta el codigo al menos una vez.
         }else{
             alert("El monto ingresado no es válido.");
         }
-    }else{
-        alert("Invalido");
     }
-}while( decisionDelUsuario != 2){
-    alert("Gracias por usar la calculadora de divisas.");
-}
+}while( decisionDelUsuario != 2);
+alert("Gracias por usar la calculadora de divisas.");
 
-console.log(arrayUSD)
+console.log(arrayUSD);
